b91bd5050553e27324dde44694b9bcd04068bbda
FaisalHasanEmon <faisalhasanemon@gmail.com>
2026-03-25 23:53:17 +0600

    furnetia video component added

---
+++ src/components/furnessia-video/FurnessiaVideo.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import video_thumb from "/assets/furnessia-video/video_thumb.png";
+import play_button from "/assets/furnessia-video/play_button.png";
+
+const FurnessiaVideo = () => {
+    const [playVideo, setPlayVideo] = useState(false);
+
+    return (
+        <section className="max-w-480 mx-auto px-4 sm:px-6 lg:px-20 lg:py-[96px] py-8 sm:py-12 ">
+            <div className="h-full w-full  space-y-12">
+                <div className="flex w-full justify-between items-baseline uppercase">
+                    <h2 className="font-familjen text-5xl md:text-[96px] lg:text-[150px] font-bold text-[#030712]">
+                        Furnessia
+                    </h2>
+                    <p className="text-xl md:text-3xl lg:text-6xl font-albert font-semibold lg:font-medium text-[#4A5565] uppercase">
+                        on the go
+                    </p>
+                </div>
+
+                <div className="flex-1 aspect-video overflow-hidden max-w-full mx-auto rounded-[48px] relative  cursor-pointer">
+
+                    {!playVideo ? (
+                        <div
+                            className="w-full h-full relative"
+                            onClick={() => setPlayVideo(true)}
+                        >
+                            <img
+                                src={video_thumb}
+                                alt="Video thumbnail"
+                                className="w-full h-full object-cover"
+                            />
+
+                            {/* Play Button */}
+                            <div className="absolute inset-0 flex items-center justify-center">
+                                <figure>
+                                    <img src={play_button} alt="Play button" className="h-[52px] w-[52px] md:h-[100px] md:w-[100px] lg:h-[180px] lg:w-[180px]" />
+                                </figure>
+
+                            </div>
+                        </div>
+                    ) : (
+                        <iframe
+                            className="w-full h-full"
+                            src="https://www.youtube.com/embed/S-A0qLTxf-U?autoplay=1&rel=0&modestbranding=1"
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen
+                        ></iframe>
+                    )}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default FurnessiaVideo;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@
 import Banner from '../../components/banner/Banner'
 import Cta from '../../components/cta/Cta'
 import EMIBanner from '../../components/emi-banner/EMIBanner'
+import FurnessiaVideo from '../../components/furnessia-video/FurnessiaVideo'
 
 const Home = () => {
   return (
     <>
       <Banner />
       <EMIBanner />
+      <FurnessiaVideo />
       <Cta />
     </>
 
